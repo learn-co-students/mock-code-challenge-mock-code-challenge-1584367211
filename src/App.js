a68a13014b4c19d214029228501c74c5e36479b4
JoelBly <jtb6179@g.rit.edu>
2020-03-16 12:19:00 -0400

    I failed

--- src/App.js
+++ src/App.js
@@ -8,13 +8,37 @@ import SearchBar from './Components/SearchBar'
 
 class App extends React.Component {
 
+  state = {
+    annoyingHippys: [],
+    seekingHippys: ''
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:4000/planeteers')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({  
+          annoyingHippys: data
+        })
+      })
+  }
+  
+  filterHippys = (event) => {
+
+    this.setState({
+      seekingHippys: event.target.value
+    })
+  }
+  
   render(){
+
+
     return (
       <div>
         <Header />
-        <SearchBar />
+        <SearchBar captainPlanetWasATerribleTVShow={this.filterHippys} />
         <RandomButton/>
-        <PlaneteersContainer />
+        <PlaneteersContainer hippys={this.state.annoyingHippys}/>
       </div>
     );
   }
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 class SearchBar extends React.Component {
 
   render() {
+
     return (
       <div className="search">
-       <input type="text" className="searchTerm" placeholder="Who would you like to search for?"/>
+        <input type="text" className="searchTerm" placeholder="Who would you like to search for?" onChange={this.props.captainPlanetWasATerribleTVShow}/>
       </div>
     );
   }
